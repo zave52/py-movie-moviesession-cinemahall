cf6970ca92ae0041fdf65cea72a5f53de8e6d785
zakhar <zaharsavchen@gmail.com>
2024-09-27 23:20:17 +0300

    refactored to use get_movie_session_by_id

--- services/movie_session.py
+++ services/movie_session.py
@@ -37,7 +37,7 @@ def update_movie_session(
     movie_id: int = None,
     cinema_hall_id: int = None
 ) -> None:
-    movie_session = MovieSession.objects.get(id=session_id)
+    movie_session = get_movie_session_by_id(session_id)
 
     if show_time:
         movie_session.show_time = show_time
@@ -50,4 +50,4 @@ def update_movie_session(
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    MovieSession.objects.get(id=session_id).delete()
+    get_movie_session_by_id(session_id).delete()
